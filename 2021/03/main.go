7eca5cb8a1d9a08e33e9204a13a045ab0412f78c
sakisv <me@sakisv.net>
2021-12-03 16:06:57 +0200

    2021 Day 3 - part 2

--- 2021/03/main.go
+++ 2021/03/main.go
@@ -16,6 +16,60 @@ func getInput() []string {
 	return strings.Split(strings.TrimSpace(string(contents)), "\n")
 }
 
+func getOxygenGeneratorRating(input []string, index int) string {
+	input_length := len(input)
+	if input_length == 1 {
+		return input[0]
+	}
+
+	bit_count := 0
+	for _, line := range input {
+		bit, _ := strconv.Atoi(string(line[index]))
+		bit_count += bit
+	}
+
+	desired_bit := "1"
+	if bit_count < input_length / 2 {
+		desired_bit = "0"
+	}
+
+	var next_input []string
+	for _, line := range input {
+		if string(line[index]) == desired_bit {
+			next_input = append(next_input, line)
+		}
+	}
+
+	return getOxygenGeneratorRating(next_input, index + 1)
+}
+
+func getCO2ScrubberRating(input []string, index int) string {
+	input_length := len(input)
+	if input_length == 1 {
+		return input[0]
+	}
+
+	bit_count := 0
+	for _, line := range input {
+		bit, _ := strconv.Atoi(string(line[index]))
+		bit_count += bit
+	}
+
+	desired_bit := "0"
+	if bit_count < input_length / 2 {
+		desired_bit = "1"
+	}
+
+	var next_input []string
+	for _, line := range input {
+		if string(line[index]) == desired_bit {
+			next_input = append(next_input, line)
+		}
+	}
+
+	return getCO2ScrubberRating(next_input, index + 1)
+}
+
 func main() {
 	var bit_counts = [12]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 
@@ -47,6 +101,7 @@ func main() {
 		}
 	}
 
+	log.Print("Part 1")
 	log.Print(bit_counts)
 	log.Print(strings.Join(gamma_rate, ""))
 	log.Print(strings.Join(epsilon_rate, ""))
@@ -56,4 +111,15 @@ func main() {
 	log.Print(gr_decimal)
 	log.Print(er_decimal)
 	log.Print(gr_decimal * er_decimal)
+
+	log.Print("Part 2")
+	oxygen_generator_rating := getOxygenGeneratorRating(input, 0)
+	co2_scrubber_rating := getCO2ScrubberRating(input, 0)
+
+	oxygen_generator_rating_decimal, _ := strconv.ParseInt(oxygen_generator_rating, 2, 16)
+	co2_scrubber_rating_decimal, _ := strconv.ParseInt(co2_scrubber_rating, 2, 16)
+
+	log.Print(oxygen_generator_rating_decimal)
+	log.Print(co2_scrubber_rating_decimal)
+	log.Print(oxygen_generator_rating_decimal * co2_scrubber_rating_decimal)
 }
