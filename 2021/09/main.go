7e0975f128ad332f7c56a0164b1ca0032adf48b7
sakisv <me@sakisv.net>
2021-12-24 18:33:46 +0200

    2021 Day 9 - part 2

--- 2021/09/main.go
+++ 2021/09/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -55,6 +57,51 @@ func (c *Cell) isLowerThanNeighbors(field [][]int) bool {
 	return true
 }
 
+func (c *Cell) getMapKey() string {
+	return fmt.Sprintf("%v-%v-%v", c.row, c.column, c.value)
+}
+
+func (c *Cell) visitNeighbors(field [][]int, visitedNeighbors map[string]bool) {
+	if c.value == 9 {
+		return
+	}
+
+	if isVisited, exists := visitedNeighbors[c.getMapKey()]; exists && isVisited {
+		return
+	}
+
+	visitedNeighbors[c.getMapKey()] = true
+
+	if c.row - 1 >= 0 {
+		nextCell := Cell{c.row - 1, c.column, field[c.row - 1][c.column]}
+		nextCell.visitNeighbors(field, visitedNeighbors)
+	}
+
+	if c.row + 1 < len(field) {
+		nextCell := Cell{c.row + 1, c.column, field[c.row + 1][c.column]}
+		nextCell.visitNeighbors(field, visitedNeighbors)
+	}
+
+	if c.column - 1 >= 0 {
+		nextCell := Cell{c.row, c.column - 1, field[c.row][c.column - 1]}
+		nextCell.visitNeighbors(field, visitedNeighbors)
+	}
+	if c.column + 1 < len(field[c.row]) {
+		nextCell := Cell{c.row, c.column + 1, field[c.row][c.column + 1]}
+		nextCell.visitNeighbors(field, visitedNeighbors)
+	}
+
+}
+
+func (c *Cell) findBasinSize(field [][]int) int {
+	visitedCells := make(map[string]bool)
+	c.visitNeighbors(field, visitedCells)
+
+	basinSize := len(visitedCells)
+
+	return basinSize
+}
+
 func main() {
 	filename := "input.txt"
 	if len(os.Args) > 1 {
@@ -63,14 +110,19 @@ func main() {
 	input := getInput(filename)
 
 	riskLevelSum := 0
+	var basinSizes []int
 	for row := 0; row < len(input); row++ {
 		for column := 0; column < len(input[row]); column++ {
 			c := Cell{row, column, input[row][column]}
 			if c.isLowerThanNeighbors(input) {
 				riskLevelSum += c.value + 1
+				basinSizes = append(basinSizes, c.findBasinSize(input))
 			}
 		}
 	}
 
+	sort.Ints(basinSizes)
+	l := len(basinSizes)
 	log.Print(riskLevelSum)
+	log.Print(basinSizes[l - 1] * basinSizes[l - 2] * basinSizes[l - 3])
 }
