b34cc0ebe6df49cf36120b709ccea156dab44d64
sakisv <me@sakisv.net>
2022-11-29 00:01:15 +0200

    2020 Day 5 - Part 1

---
+++ 2020/05/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+func getInput() []string {
+	contents, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lines := strings.Split(strings.TrimSpace(string(contents)), "\n")
+
+	return lines
+}
+
+func recurseRows(directions []string, min, max int) int {
+	//log.Print(directions, min, max)
+	if len(directions) == 1 {
+		if directions[0] == "F" {
+			return min
+		}
+		return max
+	}
+
+	seatRange := max - min
+	halfRange := seatRange / 2
+	if directions[0] == "F" {
+		return recurseRows(directions[1:], min, max-halfRange-1)
+	}
+
+	if directions[0] == "B" {
+		return recurseRows(directions[1:], min+halfRange+1, max)
+	}
+
+	return -1
+}
+
+func recurseCols(directions []string, min, max int) int {
+	//log.Print(directions, min, max)
+	if len(directions) == 1 {
+		if directions[0] == "L" {
+			return min
+		}
+		return max
+	}
+
+	seatRange := max - min
+	halfRange := seatRange / 2
+	if directions[0] == "L" {
+		return recurseCols(directions[1:], min, max-halfRange-1)
+	}
+
+	if directions[0] == "R" {
+		return recurseCols(directions[1:], min+halfRange+1, max)
+	}
+
+	return -1
+}
+
+func getRow(v []string) int {
+	return recurseRows(v, 0, 127)
+}
+
+func getCol(v []string) int {
+	return recurseCols(v, 0, 7)
+}
+
+func main() {
+	seats := getInput()
+
+	maxSeatId := 0
+	for _, s := range seats {
+		row := getRow(strings.Split(s[:7], ""))
+		col := getCol(strings.Split(s[7:], ""))
+		seatId := (row * 8) + col
+
+		log.Print(row, col)
+		if seatId > maxSeatId {
+			maxSeatId = seatId
+		}
+	}
+
+	log.Print(maxSeatId)
+}
