8a20aac864f4fe91ca7e9052b25026d59b44d98e
sakisv <me@sakisv.net>
2022-12-09 23:29:29 +0200

    2022 Day 9 - Part 1
    Took me a while to realize that the `count` should be `Abs`ed

--- 2022/09/main.go
+++ 2022/09/main.go
@@ -33,7 +33,9 @@ type Position struct {
 }
 
 func (p *Position) toString() string {
-	return string(p.x) + "-" + string(p.y)
+	xString := strconv.Itoa(p.x)
+	yString := strconv.Itoa(p.y)
+	return xString + "-" + yString
 }
 
 func uniqPositions(positions []Position) map[string]bool {
@@ -44,75 +46,61 @@ func uniqPositions(positions []Position) map[string]bool {
 	return uniqPositions
 }
 
-func simulateHead(input []string) []Position {
+func runMoves(input []string) ([]Position, []Position) {
 	//start from 0,0
-	positions := make([]Position, 0)
 	curPosition := Position{0, 0}
-	positions = append(positions, curPosition)
+
+	headPositions := make([]Position, 0)
+	tailPositions := make([]Position, 0)
+	headPositions = append(headPositions, curPosition)
+	tailPositions = append(tailPositions, curPosition)
+
+	direction := ""
 	for _, line := range input {
 		split := strings.Fields(line)
-		direction := split[0]
+		direction = split[0]
 		count, _ := strconv.Atoi(split[1])
 
-		for i := 0; i < count; i++ {
+		for i := 0; i < int(math.Abs(float64(count))); i++ {
+			newPosition := Position{}
 			if direction == "U" {
-				curPosition = Position{curPosition.x, curPosition.y + 1}
+				newPosition = Position{curPosition.x, curPosition.y + 1}
 			}
 			if direction == "R" {
-				curPosition = Position{curPosition.x + 1, curPosition.y}
+				newPosition = Position{curPosition.x + 1, curPosition.y}
 			}
 			if direction == "D" {
-				curPosition = Position{curPosition.x, curPosition.y - 1}
+				newPosition = Position{curPosition.x, curPosition.y - 1}
 			}
 			if direction == "L" {
-				curPosition = Position{curPosition.x - 1, curPosition.y}
+				newPosition = Position{curPosition.x - 1, curPosition.y}
 			}
-			positions = append(positions, curPosition)
+			log.Print("Moving from ", curPosition, " to ", newPosition)
+			headPositions = append(headPositions, newPosition)
+
+			// if the tail has moved too far away then append the previous position of the head
+			// for example:
+			// t(0,0), h(0,0) -> t(0,0), h(1, 0) -> t(0,0), h(2,0)
+			// in the third moment we add the previous position of the head to
+			// the tail positions, (1,0)
+			//
+			// if head was to move right 1, up 2, from the same starting position (0,0)
+			// t(0,0), h(1,0) -> t(0,0), h(1,1) -> t(0,0), h(1,2)
+			// again at this point we add the previous position of the head
+			// (1,1) to the tail's positions
+			lastTailPosition := tailPositions[len(tailPositions)-1]
+			log.Print("Last tail position: ", lastTailPosition)
+			if positionsAreFarAway(lastTailPosition, newPosition, 2) {
+				secondTolastHeadPosition := headPositions[len(headPositions)-2]
+				log.Print("==> Tail is far away, moving to ", secondTolastHeadPosition)
+				tailPositions = append(tailPositions, secondTolastHeadPosition)
+			}
+			curPosition = newPosition
 		}
 
 	}
 
-	return positions
-}
-
-func sameAxisOppositeDirection(d1, d2 string) bool {
-	if (d1 == "U" && d2 == "D") || (d1 == "D" && d2 == "U") {
-		return true
-	}
-
-	if (d1 == "R" && d2 == "L") || (d1 == "L" && d2 == "R") {
-		return true
-	}
-
-	return false
-}
-
-func differentAxisSameDirection(d1, d2 string) bool {
-	// same direction means "increasing" the coordinate, i.e. up or right
-
-	if (d1 == "U" && d2 == "R") || (d1 == "R" && d2 == "U") {
-		return true
-	}
-
-	if (d1 == "L" && d2 == "D") || (d1 == "D" && d2 == "L") {
-		return true
-	}
-
-	return false
-}
-
-func differentAxisOppositeDirection(d1, d2 string) bool {
-	// opposite direction means from increasing to decreasing or vice versa
-
-	if (d1 == "U" && d2 == "L") || (d1 == "L" && d2 == "U") {
-		return true
-	}
-
-	if (d1 == "D" && d2 == "R") || (d1 == "R" && d2 == "D") {
-		return true
-	}
-
-	return false
+	return headPositions, tailPositions
 }
 
 func positionsAreFarAway(p1, p2 Position, farAway int) bool {
@@ -127,72 +115,15 @@ func positionsAreFarAway(p1, p2 Position, farAway int) bool {
 	return false
 }
 
-func simulateTail(input []string) []Position {
-	//start from 0,0
-	positions := make([]Position, 0)
-	curPosition := Position{0, 0}
-	positions = append(positions, curPosition)
-	curDirection := ""
-
-	for _, line := range input {
-		split := strings.Fields(line)
-		nextDirection := split[0]
-		count, _ := strconv.Atoi(split[1])
-
-		if sameAxisOppositeDirection(curDirection, nextDirection) {
-			// reduce one for overlapping and another for going to the opposite
-			// direction
-			count -= 2
-		} else if differentAxisSameDirection(curDirection, nextDirection) {
-			// ignore the first move because we're already remain "in contact"
-			// (diagonally)
-			count -= 1
-		} else if differentAxisOppositeDirection(curDirection, nextDirection) {
-			count -= 3
-		}
-
-		for i := 0; i < count; i++ {
-			if nextDirection == "U" {
-				curPosition = Position{curPosition.x, curPosition.y + 1}
-			}
-			if nextDirection == "R" {
-				curPosition = Position{curPosition.x + 1, curPosition.y}
-			}
-			if nextDirection == "D" {
-				curPosition = Position{curPosition.x, curPosition.y - 1}
-			}
-			if nextDirection == "L" {
-				curPosition = Position{curPosition.x - 1, curPosition.y}
-			}
-			positions = append(positions, curPosition)
-			curDirection = nextDirection
-		}
-
-	}
-
-	return positions
-}
-
 func main() {
 	input := getInput(getFilename())
 
 	// get the positions the head will move
-	headPositions := simulateHead(input)
+	headPositions, tailPositions := runMoves(input)
 	uniqHeadPositions := uniqPositions(headPositions)
+	uniqTailPositions := uniqPositions(tailPositions)
 	log.Print(len(headPositions))
 	log.Print(len(uniqHeadPositions))
-
-	// the tail does 1 fewer move than the head
-	// e.g. if the head goes right 4 positions, the tail only goes 3
-	//
-	// this is also true if the head changes direction vertically
-	// e.g. the head goes up/down 1 position, the tail is still close enough
-	//
-	// if the head moves in the same axies, but to the opposite direction then
-	// the tail follows if the head moves more than 2 moves:
-	// the first will overlap with tail and the second will still be near enough
-	tailPositions := simulateTail(input)
-	uniqTailPositions := uniqPositions(tailPositions)
 	log.Print(len(tailPositions))
 	log.Print(len(uniqTailPositions))
 }
