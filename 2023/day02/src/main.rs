4f7ef0b61b5a68c486a9f455f977873d2f4950d3
sakisv <me@sakisv.net>
2023-12-02 20:14:39 +0200

    2023 Day 2 - Add failing test for part2

--- 2023/day02/src/main.rs
+++ 2023/day02/src/main.rs
@@ -53,10 +53,20 @@ fn solve_part1(input: &Vec<String>) -> i32 {
 }
 
 
+fn solve_part2(input: &Vec<String>) -> i32 {
+    let cubes_power = 0;
+    cubes_power
+}
+
+
 fn main() {
     let input = get_input("input.txt");
+
     let part1 = solve_part1(&input);
     println!("Part 1: {part1}");
+
+    let part2 = solve_part2(&input);
+    println!("Part 2: {part2}");
 }
 
 #[cfg(test)]
@@ -73,5 +83,19 @@ mod tests {
 
         let result = super::solve_part1(&input);
         assert_eq!(result, 8);
+    }
+
+    #[test]
+    fn solve_part2() {
+        let input = vec![
+            String::from("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"),
+            String::from("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"),
+            String::from("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"),
+            String::from("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"),
+            String::from("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"),
+        ];
+
+        let result = super::solve_part2(&input);
+        assert_eq!(result, 2286);
     }
 }
