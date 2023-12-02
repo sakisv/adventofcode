f6323fd6aadf3e47e03eb6e87e6575b7434d0fb7
sakisv <me@sakisv.net>
2023-12-02 20:40:40 +0200

    2023 Day 2 - Part 2

--- 2023/day02/src/main.rs
+++ 2023/day02/src/main.rs
@@ -65,7 +65,28 @@ fn solve_part1(input: &Vec<String>) -> i32 {
 
 
 fn solve_part2(input: &Vec<String>) -> i32 {
-    let cubes_power = 0;
+    let mut cubes_power = 0;
+    for line in input {
+        let (game_number, color_picks) = get_game_number_and_color_picks(line);
+        let color_counts = get_color_counts(color_picks);
+
+        let mut max_red = 1;
+        let mut max_green = 1;
+        let mut max_blue = 1;
+        for (count, color) in color_counts {
+            if color == "red" && count > max_red {
+                max_red = count;
+            }
+            if color == "green" && count > max_green {
+                max_green = count;
+            }
+            if color == "blue" && count > max_blue {
+                max_blue = count;
+            }
+        }
+
+        cubes_power = cubes_power + (max_red * max_green * max_blue);
+    }
     cubes_power
 }
 
