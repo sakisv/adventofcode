566d5a5e6ca270971139f7a0d9905202c7494f0c
sakisv <me@sakisv.net>
2023-12-04 19:57:41 +0200

    2023 Day 4 - wip

--- 2023/day04/src/main.rs
+++ 2023/day04/src/main.rs
@@ -44,6 +44,12 @@ fn solve_part1(input: &Vec<String>) -> i32 {
     sum
 }
 
+fn solve_part2(input: &Vec<String>) -> i32 {
+    let mut sum = 0;
+
+    sum
+}
+
 fn main() {
     let input = get_input("input.txt");
 
@@ -66,5 +72,20 @@ mod tests {
 
         let result = super::solve_part1(&input);
         assert_eq!(result, 13);
+    }
+
+    #[test]
+    fn solve_part2() {
+        let input = vec![
+            String::from("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"),
+            String::from("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19"),
+            String::from("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1"),
+            String::from("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83"),
+            String::from("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36"),
+            String::from("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"),
+        ];
+
+        let result = super::solve_part2(&input);
+        assert_eq!(result, 30);
     }
 }
