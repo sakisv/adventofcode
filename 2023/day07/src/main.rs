b5b9dfbdb59f9c823ca4c59d5eb060a409851038
sakisv <me@sakisv.net>
2023-12-08 16:33:42 +0000

    Day 7 - Part 2 [WiP] Add flags for part2
    This will allow different logic for the 2 parts

--- 2023/day07/src/main.rs
+++ 2023/day07/src/main.rs
@@ -16,6 +16,7 @@ struct Hand {
     combo: String,
     hand_type: HandType,
     groups: HashMap<char, Vec<char>>,
+    part2: bool,
 }
 
 impl Hand {
@@ -51,10 +52,15 @@ impl Hand {
         ht
     }
 
-    fn new(cards: &str) -> Self {
+    fn get_hand_type_part2(groups: &HashMap<char, Vec<char>>) -> HandType {
+           let mut ht = HandType::FiveOfKind;
+           ht
+    }
+
+    fn new(cards: &str, part2: bool) -> Self {
         let groups = Self::find_groups(cards);
-        let ht = Self::get_hand_type(&groups);
-        Hand { combo: String::from(cards), hand_type: ht, groups: groups }
+        let ht = if part2 { Self::get_hand_type_part2(&groups) } else { Self::get_hand_type(&groups) } ;
+        Hand { combo: String::from(cards), hand_type: ht, groups: groups, part2: part2 }
     }
 }
 
@@ -77,40 +83,44 @@ impl Eq for Hand {}
 
 impl Ord for Hand {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        if self.hand_type > other.hand_type {
-            return Ordering::Greater
-        }
-        if self.hand_type < other.hand_type {
-            return Ordering::Less
-        }
-        if self.hand_type == other.hand_type {
-            let card_values = HashMap::from([
-                ('A', 14),
-                ('K', 13),
-                ('Q', 12),
-                ('J', 11),
-                ('T', 10),
-                ('9', 9),
-                ('8', 8),
-                ('7', 7),
-                ('6', 6),
-                ('5', 5),
-                ('4', 4),
-                ('3', 3),
-                ('2', 2),
-            ]);
-            for i in 0..self.combo.len() {
-                let c1 = self.combo.chars().nth(i).unwrap();
-                let c2 = other.combo.chars().nth(i).unwrap();
+        if ! self.part2 {
+            if self.hand_type > other.hand_type {
+                return Ordering::Greater
+            }
+            if self.hand_type < other.hand_type {
+                return Ordering::Less
+            }
+            if self.hand_type == other.hand_type {
+                let card_values = HashMap::from([
+                    ('A', 14),
+                    ('K', 13),
+                    ('Q', 12),
+                    ('J', 11),
+                    ('T', 10),
+                    ('9', 9),
+                    ('8', 8),
+                    ('7', 7),
+                    ('6', 6),
+                    ('5', 5),
+                    ('4', 4),
+                    ('3', 3),
+                    ('2', 2),
+                ]);
+                for i in 0..self.combo.len() {
+                    let c1 = self.combo.chars().nth(i).unwrap();
+                    let c2 = other.combo.chars().nth(i).unwrap();
 
-                if card_values.get(&c1).unwrap() > card_values.get(&c2).unwrap() {
-                    return Ordering::Greater;
-                } else if card_values.get(&c1).unwrap() < card_values.get(&c2).unwrap() {
-                    return Ordering::Less;
+                    if card_values.get(&c1).unwrap() > card_values.get(&c2).unwrap() {
+                        return Ordering::Greater;
+                    } else if card_values.get(&c1).unwrap() < card_values.get(&c2).unwrap() {
+                        return Ordering::Less;
+                    }
                 }
             }
+            return Ordering::Equal;
+        } else {
+            return Ordering::Equal;
         }
-        Ordering::Equal
     }
 }
 
@@ -121,10 +131,10 @@ struct Round {
 }
 
 impl Round {
-    fn new(line: &str) -> Self {
+    fn new(line: &str, part2: bool) -> Self {
         let splits: Vec<&str> = line.split_ascii_whitespace().collect();
 
-        Round { hand: Hand::new(splits[0]), bid: splits[1].parse().unwrap() }
+        Round { hand: Hand::new(splits[0], false), bid: splits[1].parse().unwrap() }
     }
 }
 
@@ -143,14 +153,34 @@ fn solve_part1(input: &Vec<String>) -> i32 {
     let mut rounds: Vec<Round> = vec![];
 
     for line in input {
-        rounds.push(Round::new(line))
+        rounds.push(Round::new(line, false))
     }
 
     rounds.sort_by(|el1, el2| el1.hand.cmp(&el2.hand));
 
     for (i, v) in rounds.iter().enumerate() {
         let ii: i32 = i.try_into().unwrap();
-        println!("Hand {}: {} - Bid: {}", i+1, v.hand.combo, v.bid);
+        //println!("Hand {}: {} - Bid: {}", i+1, v.hand.combo, v.bid);
+        winnings += (ii + 1) * v.bid;
+    }
+
+    winnings
+}
+
+fn solve_part2(input: &Vec<String>) -> i32 {
+    let mut winnings = 0;
+
+    let mut rounds: Vec<Round> = vec![];
+
+    for line in input {
+        rounds.push(Round::new(line, true))
+    }
+
+    rounds.sort_by(|el1, el2| el1.hand.cmp(&el2.hand));
+
+    for (i, v) in rounds.iter().enumerate() {
+        let ii: i32 = i.try_into().unwrap();
+        //println!("Hand {}: {} - Bid: {}", i+1, v.hand.combo, v.bid);
         winnings += (ii + 1) * v.bid;
     }
 
@@ -162,6 +192,9 @@ fn main() {
 
     let part1 = solve_part1(&input);
     println!("Part 1: {part1}");
+
+    let part2 = solve_part2(&input);
+    println!("Part 2: {part2}");
 }
 
 #[cfg(test)]
@@ -181,21 +214,34 @@ mod tests {
         let result = super::solve_part1(&input);
         assert_eq!(result, 6440);
     }
+
+    fn solve_part2() {
+        let input = vec![
+            String::from("32T3K 765"),
+            String::from("T55J5 684"),
+            String::from("KK677 28"),
+            String::from("KTJJT 220"),
+            String::from("QQQJA 483"),
+        ];
+
+        let result = super::solve_part2(&input);
+        assert_eq!(result, 5905);
+    }
     #[test]
-    fn compare_hands() {
-        let h1 = Hand::new("KK677");
-        let h2 = Hand::new("KTJJT");
-        let h3 = Hand::new("32T3K");
+    fn compare_hands_part1() {
+        let h1 = Hand::new("KK677", false);
+        let h2 = Hand::new("KTJJT", false);
+        let h3 = Hand::new("32T3K", false);
         assert!(h1 > h2);
         assert!(h2 > h3);
         assert!(h1 > h3);
 
-        let h4 = Hand::new("33332");
-        let h5 = Hand::new("2AAAA");
+        let h4 = Hand::new("33332", false);
+        let h5 = Hand::new("2AAAA", false);
         assert!(h4 > h5);
 
-        let h6 = Hand::new("77888");
-        let h7 = Hand::new("77788");
+        let h6 = Hand::new("77888", false);
+        let h7 = Hand::new("77788", false);
         assert!(h6 > h7);
     }
 
