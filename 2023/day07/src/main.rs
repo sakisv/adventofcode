807b5a4e3567a4ef8bdaaae8a7126b632c32b78b
sakisv <me@sakisv.net>
2024-01-14 21:42:24 +0200

    2023 - Day 7 part 2
    Most of the logic was already there from part 1 and it didn't require
    huge adjustments.
    
    Code could have been a bit DRY-er but /shrug

--- 2023/day07/src/main.rs
+++ 2023/day07/src/main.rs
@@ -53,14 +53,38 @@ impl Hand {
     }
 
     fn get_hand_type_part2(groups: &HashMap<char, Vec<char>>) -> HandType {
-           let mut ht = HandType::FiveOfKind;
-           ht
+        // If there are no Joker cards, then reuse previous approach
+        if ! groups.contains_key(&'J') {
+            return Self::get_hand_type(groups);
+        }
+
+        let mut cloned_groups = groups.clone();
+        let j_group = cloned_groups.remove(&'J').unwrap();
+        let j_count = j_group.len();
+
+        let group_count = cloned_groups.len();
+        let group_v: Vec<&Vec<char>> = cloned_groups.values().collect();
+        let mut ht = HandType::HighCard;
+
+        if group_count == 4 {
+            ht = HandType::OnePair; // actually 5 groups: 4 other cards + 1 Joker => HighCard becomes OnePair
+        } else if group_count == 3 { // actually 4 groups - we add the jokers to the max_v in order to get the correct type
+            let max_v = group_v.into_iter().map(|v| v.len()).max().unwrap();
+            ht = if max_v + j_count == 3 { HandType::ThreeOfKind } else { HandType::TwoPair };
+        } else if group_count == 2 { // actually 3 groups - but same as before
+            let max_v = group_v.into_iter().map(|v| v.len()).max().unwrap();
+            ht = if max_v + j_count == 4 { HandType::FourOfKind } else { HandType::FullHouse };
+        } else { // 1 or 0 groups - either FourOfKind becomes FiveOfKind or we have 5 Jokers so we are at FiveOfKind anyway
+           ht = HandType::FiveOfKind
+        }
+
+        ht
     }
 
     fn new(cards: &str, part2: bool) -> Self {
         let groups = Self::find_groups(cards);
         let ht = if part2 { Self::get_hand_type_part2(&groups) } else { Self::get_hand_type(&groups) } ;
-        Hand { combo: String::from(cards), hand_type: ht, groups: groups, part2: part2 }
+        Hand { combo: String::from(cards), hand_type: ht, groups, part2 }
     }
 }
 
@@ -83,44 +107,43 @@ impl Eq for Hand {}
 
 impl Ord for Hand {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        if ! self.part2 {
-            if self.hand_type > other.hand_type {
-                return Ordering::Greater
-            }
-            if self.hand_type < other.hand_type {
-                return Ordering::Less
-            }
-            if self.hand_type == other.hand_type {
-                let card_values = HashMap::from([
-                    ('A', 14),
-                    ('K', 13),
-                    ('Q', 12),
-                    ('J', 11),
-                    ('T', 10),
-                    ('9', 9),
-                    ('8', 8),
-                    ('7', 7),
-                    ('6', 6),
-                    ('5', 5),
-                    ('4', 4),
-                    ('3', 3),
-                    ('2', 2),
-                ]);
-                for i in 0..self.combo.len() {
-                    let c1 = self.combo.chars().nth(i).unwrap();
-                    let c2 = other.combo.chars().nth(i).unwrap();
+        let mut card_values = HashMap::from([
+            ('A', 14),
+            ('K', 13),
+            ('Q', 12),
+            ('J', 11),
+            ('T', 10),
+            ('9', 9),
+            ('8', 8),
+            ('7', 7),
+            ('6', 6),
+            ('5', 5),
+            ('4', 4),
+            ('3', 3),
+            ('2', 2),
+        ]);
+        if self.part2 {
+            card_values.insert('J', 1);
+        }
+        if self.hand_type > other.hand_type {
+            return Ordering::Greater
+        }
+        if self.hand_type < other.hand_type {
+            return Ordering::Less
+        }
+        if self.hand_type == other.hand_type {
+            for i in 0..self.combo.len() {
+                let c1 = self.combo.chars().nth(i).unwrap();
+                let c2 = other.combo.chars().nth(i).unwrap();
 
-                    if card_values.get(&c1).unwrap() > card_values.get(&c2).unwrap() {
-                        return Ordering::Greater;
-                    } else if card_values.get(&c1).unwrap() < card_values.get(&c2).unwrap() {
-                        return Ordering::Less;
-                    }
+                if card_values.get(&c1).unwrap() > card_values.get(&c2).unwrap() {
+                    return Ordering::Greater;
+                } else if card_values.get(&c1).unwrap() < card_values.get(&c2).unwrap() {
+                    return Ordering::Less;
                 }
             }
-            return Ordering::Equal;
-        } else {
-            return Ordering::Equal;
         }
+        return Ordering::Equal;
     }
 }
 
@@ -134,7 +157,7 @@ impl Round {
     fn new(line: &str, part2: bool) -> Self {
         let splits: Vec<&str> = line.split_ascii_whitespace().collect();
 
-        Round { hand: Hand::new(splits[0], false), bid: splits[1].parse().unwrap() }
+        Round { hand: Hand::new(splits[0], part2), bid: splits[1].parse().unwrap() }
     }
 }
 
@@ -180,7 +203,7 @@ fn solve_part2(input: &Vec<String>) -> i32 {
 
     for (i, v) in rounds.iter().enumerate() {
         let ii: i32 = i.try_into().unwrap();
-        //println!("Hand {}: {} - Bid: {}", i+1, v.hand.combo, v.bid);
+        //println!("Hand {}: {} ({:?}) - Bid: {}", i+1, v.hand.combo, v.hand.hand_type, v.bid);
         winnings += (ii + 1) * v.bid;
     }
 
@@ -215,6 +238,7 @@ mod tests {
         assert_eq!(result, 6440);
     }
 
+    #[test]
     fn solve_part2() {
         let input = vec![
             String::from("32T3K 765"),
@@ -226,6 +250,21 @@ mod tests {
 
         let result = super::solve_part2(&input);
         assert_eq!(result, 5905);
+    }
+
+    #[test]
+    fn find_hand_type_part2() {
+        let h1 = Hand::new("32T3K", true);
+        let h2 = Hand::new("T55J5", true);
+        let h3 = Hand::new("KK677", true);
+        let h4 = Hand::new("KTJJT", true);
+        let h5 = Hand::new("QQQJA", true);
+
+        assert_eq!(h1.hand_type, crate::HandType::OnePair);
+        assert_eq!(h2.hand_type, crate::HandType::FourOfKind);
+        assert_eq!(h3.hand_type, crate::HandType::TwoPair);
+        assert_eq!(h4.hand_type, crate::HandType::FourOfKind);
+        assert_eq!(h5.hand_type, crate::HandType::FourOfKind);
     }
     #[test]
     fn compare_hands_part1() {
